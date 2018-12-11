4cc73e9657c173117fbb8ced6ee529be487b0727
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-10 17:33:25 -0800

    Complete lecture 62

--- section-05/app.js
+++ section-05/app.js
@@ -1,18 +1,20 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next(); // allows request to continue to next middleware
+app.use('/', (req, res, next) => {
+  console.log('This always runs!');
+  next();
 });
 
-app.use((req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from express</h1>');
+app.use('/add-product', (req, res, next) => {
+  console.log('Add product!');
+  res.send('<h1>Add product</h1>');
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  console.log('Welcome!');
+  res.send('<h1>Hello from express!</h1>');
+});
+
+app.listen(3000);
