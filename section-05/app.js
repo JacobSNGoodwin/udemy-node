1ec8904c72849bd6bd8f8b21df06e1a7b8c17692
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-10 17:22:54 -0800

    Add first basic middleware

--- section-05/app.js
+++ section-05/app.js
@@ -2,7 +2,17 @@ const http = require('http');
 
 const express = require('express');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer();
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  next(); // allows request to continue to next middleware
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from express</h1>');
+});
+
+const server = http.createServer(app);
 server.listen(3000);
