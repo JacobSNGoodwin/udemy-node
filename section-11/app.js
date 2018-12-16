cae137fcd42ec7cf4438f6dc7d25991ffb1023a2
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-15 19:54:39 -0800

    Lecture 160

--- section-11/app.js
+++ section-11/app.js
@@ -19,6 +19,16 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  // middleware that stores the user in the request
+  User.findById(1)
+    .then(user => {
+      req.user = user; // user is a sequelize object!
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -32,10 +42,20 @@ Product.belongsTo(User, {
 User.hasMany(Product);
 
 sequelize.sync({
-  force: true
+  force: false
 })
   .then(result => {
     // only start server if sequelize syncs correctly
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com'});
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => {
