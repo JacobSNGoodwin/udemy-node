30c5e57f935fc3f7432a9b2f29c4efaceabdcf95
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-19 10:14:49 -0800

    Lecture 189 - Fix add product bug

--- section-12/models/product.js
+++ section-12/models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
-    this._id = new mongodb.ObjectId(id); // optional fifth argument
+    this._id =  id ? new mongodb.ObjectId(id) : null; // optional fifth argument
   }
 
   save() {
