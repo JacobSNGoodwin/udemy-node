9016f8ce0161f374c77059586bf55c98e3757885
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-18 21:56:17 -0800

    Lecture 183 - get a single product for product detail page

--- section-12/models/product.js
+++ section-12/models/product.js
@@ -1,3 +1,6 @@
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
 class Product {
   constructor(title, price, imageUrl, description) {
     this.title = title;
@@ -5,28 +8,38 @@ class Product {
     this.imageUrl = imageUrl;
     this.description = description;
   }
+
+  save() {
+    const db = getDb();
+    return db.collection('products').insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products').find().toArray()
+      .then(products => {
+        // console.log(products);
+        return products;
+      })
+      .catch(err => console.log(err));
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    // next to get item at cursor
+    return db.collection('products').find({_id: new mongodb.ObjectId(prodId)}).next()
+      .then(product => {
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
 
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
 
 module.exports = Product;
--- section-12/util/database.js
+++ section-12/util/database.js
@@ -1,14 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 // wrap connection in a method to run in app.js
 const mongoConnect = (callback) => {
   MongoClient.connect('mongodb+srv://jacobgoodwin:3Fj5YDLRMn6N2bm@cluster0-yrdwb.mongodb.net/test?retryWrites=true')
   .then(client => {
     console.log('Connected!');
-    callback(client);
+    _db = client.db();
+    callback();
   })
-  .catch(err => console.log(err));
-}
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
+};
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if(_db) {
+    return _db;
+  }
+  throw 'No database found!'
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
