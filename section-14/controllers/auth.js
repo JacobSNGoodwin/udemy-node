c56c039bfd3b29171a301fde7f1af4d65f181960
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-26 10:17:14 -0800

    Lecture 242 - make sure session is updated before redirect

--- section-14/controllers/auth.js
+++ section-14/controllers/auth.js
@@ -13,7 +13,11 @@ exports.postLogin = (req, res, next) => {
     .then(user => {
       req.session.user = user;
       req.session.isLoggedIn = true;
-      res.redirect('/');
+      // save makes sure we save session before redirect since it takes a few ms
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect('/');
+      });
     })
     .catch(err => console.log(err));
 };
