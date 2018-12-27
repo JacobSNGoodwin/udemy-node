4fe3b324d6e055d1bc37ec1662681405672c7388
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-27 10:17:07 -0800

    Lecture 253 - Add login logic

--- section-15/controllers/auth.js
+++ section-15/controllers/auth.js
@@ -19,14 +19,32 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5c1c6f8bb85e733d201d3da3')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email: email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        return res.redirect('/login');
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
+
+     
     })
     .catch(err => console.log(err));
 };
