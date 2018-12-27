a82405237861ac9dcee90df1c5f110db470b2c99
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-27 11:53:43 -0800

    Get rid of isAuthenticated in individual controller methods

--- section-15/controllers/shop.js
+++ section-15/controllers/shop.js
@@ -54,8 +54,7 @@ exports.getCart = (req, res, next) => {
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
-        products: products,
-        isAuthenticated: req.session.isLoggedIn
+        products: products
       });
     })
     .catch(err => console.log(err));
@@ -115,8 +114,7 @@ exports.getOrders = (req, res, next) => {
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
-        orders: orders,
-        isAuthenticated: req.session.isLoggedIn
+        orders: orders
       });
     })
     .catch(err => console.log(err));
