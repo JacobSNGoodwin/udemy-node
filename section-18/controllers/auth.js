48d0ec9f87b3c0dca751344dab2e9657903f187c
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-01-10 09:38:02 -0800

    Save old data for invalid data on login page

--- section-18/controllers/auth.js
+++ section-18/controllers/auth.js
@@ -26,7 +26,12 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: ''
+    },
+    validationErrors: []
   });
 };
 
@@ -57,15 +62,28 @@ exports.postLogin = (req, res, next) => {
     return res.status(422).render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+      },
+      validationErrors: errors.array()
     });
   }
 
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
-        req.flash('error', 'Invalid email or password.');
-        return res.redirect('/login');
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password',
+          oldInput: {
+            email: email,
+            password: password,
+          },
+          validationErrors: [] // could add object with params key, but that shows if error is email or password related
+        });
       }
       bcrypt
         .compare(password, user.password)
@@ -78,8 +96,16 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
-          req.flash('error', 'Invalid email or password.');
-          res.redirect('/login');
+          return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Invalid email or password',
+            oldInput: {
+              email: email,
+              password: password,
+            },
+            validationErrors: [] // could add object with params key, but that shows if error is email or password related
+          });
         })
         .catch(err => {
           console.log(err);
