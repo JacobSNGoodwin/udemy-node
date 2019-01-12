11990cb2a352f7b4e5d36211c1a570fe17c38825
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-01-12 11:52:59 -0800

    Redner 500 errors with express middleware

--- main-project/controllers/admin.js
+++ main-project/controllers/admin.js
@@ -56,7 +56,6 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch(err => {
-      res.redirect('/500');
       // return res.status(500).render('admin/edit-product', {
       //   pageTitle: 'Add Product',
       //   path: '/admin/add-product',
@@ -71,6 +70,9 @@ exports.postAddProduct = (req, res, next) => {
       //   errorMessage: 'Databse operation failed. Please try again.',
       //   validationErrors: []
       // });
+      const error =  new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -95,7 +97,11 @@ exports.getEditProduct = (req, res, next) => {
         validationErrors: []
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error =  new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postEditProduct = (req, res, next) => {
